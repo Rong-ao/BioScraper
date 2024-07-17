68e0de175383d0a785a5f44369acfcac9295360a
Rong-ao Kou <kourongao@westlake.edu.cn>
2024-07-17 16:55:00 +0800

    Update and rename test.py to HMDB_scraper.py

--- HMDB_scraper.py
+++ HMDB_scraper.py
@@ -1,9 +1,23 @@
-import os
+import time
+
+print('If this is the first time to use this program on your computer, I recommend you to check whether package "requests", "BeautifulSoup" and "pandas" were installed.\n\
+      If not, run these commands to install:\n\
+      pip install requests\n\
+      pip install beautifulsoup4\n\
+      pip install lxml\n\
+      pip install pandas\n\
+      pip install urllib3\n\n')
+print('Author: Rongao Kou, Westlake University (If any problem, please check https://github.com/Rong-ao/BioScraper)\n\n')
+print('If there are not requested packages above on your computer, it will close automatically in 10 seconds.')
+time.sleep(10)
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import urllib3
 import json
+
+# For more help, see https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
               'Upgrade-Insecure-Requests': '1'}
@@ -48,18 +62,19 @@ def info_scraper(HMDB):
         s = requests.session()
         requests.packages.urllib3.disable_warnings()
         r = s.get(url, headers=header, verify=False, timeout=60)
-        if r.status_code == 200:
-            soup = BeautifulSoup(r.text, 'html.parser')
-            description = soup.find('td', {'class':"met-desc"})
-            description = description.contents[0]
-            description = description[:description.find('(PMID')]
+        r.raise_for_status()
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        description = soup.find('td', {'class':"met-desc"})
+        description = description.contents[0]
+        description = description[:description.find('(PMID')]
     except:
         description = 'Not found related information in HMDB'
 
     return description
     
 
-def integrate(in_dir, out_dir, sheet_number=0):
+def integrate(in_dir, sheet_number=0):
     if in_dir.endswith('xlsx') or in_dir.endswith('xls'):
         df = pd.read_excel(io=in_dir, header=0, index_col=None, sheet_name=sheet_number)
     elif in_dir.endswith('csv'):
@@ -68,9 +83,9 @@ def integrate(in_dir, out_dir, sheet_number=0):
     l = []
     link = []
     for cas in df['CAS']:
-        print("Searching for CAS ID: {}".format(cas))
+        print("****************** Searching for CAS ID: {} ******************".format(cas))
         if pd.isna(cas) == True:
-            info = None
+            info = 'No CAS ID'
             link_site = None
         else:
             cid = CAS_to_CID(cas)
@@ -83,49 +98,43 @@ def integrate(in_dir, out_dir, sheet_number=0):
                     link_site = 'https://pubchem.ncbi.nlm.nih.gov/compound/{}'.format(cid)
                     info = "CID: {}, but HMDBID Not found".format(cid)
                 else:
-                    print(hmdb)
+                    # print(hmdb)
                     link_site = hmdb
                     info = info_scraper(hmdb)
         print(info)
         l.append(info)
         link.append('=HYPERLINK("{}", "{}")'.format(link_site, link_site))
     s = pd.Series(l, name='Description(HMDB)')
-    print(s)
     s2 = pd.Series(link, name='Hyperlink')
-    print(s2)
     df = pd.concat([df, s, s2], axis=1)
     print(df)
-    df.to_csv(out_dir + '.csv', sep='\t', encoding='utf-8-sig')
 
-    return 
+    return df
 
 def main():
+    print('This scraper is used for searching for metabolite information from PubChem (https://pubchem.ncbi.nlm.nih.gov/) and HMDB (https://hmdb.ca/) according to CAS ID.')
     filepath = input('Please input your list file directory of metabolites (csv or xlsx format recommended, e.g. D:\\Users\\work_dir\\test.csv): ')
-    output = input('Please input your output file directory (with output name you want, e.g. D:\\Users\\work_dir\\out_test):')
+    output = input('Please input your output file directory (with output name you want, e.g. D:\\Users\\work_dir\\out_test): ')
     if filepath.endswith('xlsx') or filepath.endswith('xls'):
-        sheet = input('Which sheet do you want to search for? You can:\n\
+        sheet = input('Which sheet do you want to search for? \n\
+                      You can:\n\
                       Enter the number or sheet name, e.g. 0 OR Sheet1 for the first sheet\n\
                       Or press Enter for all sheets\n')
-        if sheet == '\n':
-            excel = pd.ExcelFile(filepath)
-            for i in excel.sheet_names:
-                integrate(filepath, output, i)
-                df = pd.read_csv(output + '.csv', header=0, index_col=None, sep='\t')
-            with pd.ExcelWriter(output + '.xlsx') as writer:
-                df.to_excel(writer, sheet_name='Sheet1', index=False)
-            os.remove(output + '.csv')
-        else:
-            integrate(filepath, output, sheet)
+        excel = pd.ExcelFile(filepath)
+        with pd.ExcelWriter(output + '.xlsx') as writer:
+            if sheet == '':
+                sheet_list = excel.sheet_names
+            else:
+                sheet_list = [sheet]
+            for i in sheet_list:
+                print("****************** Proceeding Excel Sheet: {}... ******************".format(i))
+                df = integrate(filepath, i)
+                df.to_excel(writer, sheet_name=i, index=False)
+    elif filepath.endswith('csv'):
+        df = integrate(filepath, sheet)
+        df.to_csv(output + '.csv', sep='\t', encoding='utf-8-sig')
+    else:
+        print('Wrong File Format Received! Please check your input file format or spelling.')
+
 
 main()
-# main(in_dir="C:\\Users\\DELL\\Desktop\\化合物询价.xlsx", out_dir="C:\\Users\\DELL\\Desktop\\化合物询价_sheet1", sheet_number=0)
-
-# main(in_dir="C:\\Users\\DELL\\Desktop\\化合物询价.xlsx", out_dir="C:\\Users\\DELL\\Desktop\\化合物询价_sheet2", sheet_number=1)
-
-# df1 = pd.read_csv("C:\\Users\\DELL\\Desktop\\化合物询价_sheet1.csv", header=0, index_col=None, sep='\t')
-# df2 = pd.read_csv("C:\\Users\\DELL\\Desktop\\化合物询价_sheet2.csv", header=0, index_col=None, sep='\t')
-# print(df1)
-# print(df2)
-# with pd.ExcelWriter('C:\\Users\\DELL\\Desktop\\化合物询价_HMDB.xlsx') as writer:
-#     df1.to_excel(writer, sheet_name='Sheet1', index=False)
-#     df2.to_excel(writer, sheet_name='Sheet2', index=False)
